65361dbc1b9dfe337ba55959ac8aba59a6efd87f
yossrmohammed <yossr.khatab5@gmail.com>
2024-04-22 14:34:39 +0200

    update index function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
  def index 
+    #post = Post.create({title: "t2", content: "content2"})
+    posts = Post.all
+    puts posts.each {
+        |post|
+        puts post.attributes
+    }
  end
 end
