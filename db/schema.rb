2ea01dfc24928f3832d0b838b75dfe3cd149a4c1
shimaa fathi <shimaafathi126@gmail.com>
2024-04-28 13:29:10 +0000

    add-view-files

--- db/schema.rb
+++ db/schema.rb
@@ -10,4 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema[7.1].define(version: 2024_04_28_132342) do
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "content"
+  end
 
+end
