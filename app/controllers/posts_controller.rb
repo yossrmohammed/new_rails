aa6a8f926e70f7e670d763333938a0336060f43b
shimaa fathi <shimaafathi126@gmail.com>
2024-04-28 13:00:33 +0000

    add-new-create-functions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,19 @@ class PostsController < ApplicationController
         @posts = Post.all
     end
 
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new({title:params[:post][:title], content:params[:post][:content]})
+
+        if @post.save
+            redirect_to posts_url(@post)
+        else
+            render :new, status:422
+        end
+    end
+
 
 end
