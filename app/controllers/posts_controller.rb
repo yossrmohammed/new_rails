d5d629bd6f66f55c58246da4a833aff11a710710
shimaa <shimaafathi126@gmail.com>
2024-04-29 19:22:03 +0300

    handle-delete-post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,50 +1,38 @@
 class PostsController < ApplicationController
-  def index
-     @posts=Post.all
-  end
-
-  def new 
-      @post=Post.new
-  end
-
-  def create
-
-      @post = Post.new({title: params[:post][:title], content: params[:post][:content]})
-      if @post.save 
-      redirect_to @post
-      else
-      render :new 
-      end
-
-  end
-
-  def show
-      @post=Post.find(params[:id])
-      puts "#{@post}"
-  end
-
-  def edit
-      @post = Post.find(params[:id])
-  end
-
-    def update
-        @post = Post.find(params[:id])
-        if @post.update(post_params)
-            redirect_to @post
-        else
-            render :edit
-        end
+    def index 
+       @posts = Post.all
+       #puts @posts
     end
-
-  def destroy
-      @post = post.find(params[:id]) 
+    def new 
+       @post = Post.new
+    end
+    def create
+       @post = Post.new({title: params[:post][:title], content: params[:post][:content]})
+       #puts "+++ #{params}"
+       if @post.save
+           redirect_to posts_url(@post)
+       else
+           render :new, status: 422
+       end
+    end
+    def show
+      @post = Post.find(params[:id])
+      
+    end
+    def destroy
+      @post = Post.find(params[:id]);
       @post.destroy
-      redirect_to @post
-  end
-
-end
-private
-
-def post_params
-  params.require(:post).permit(:title, :content)
-end
+      redirect_to posts_url(@post)
+    end
+    def edit
+      @post = Post.find(params[:id])
+    end
+    def update
+     @post = Post.find(params[:id])
+     if @post.update({title: params[:post][:title], content: params[:post][:content]})
+     redirect_to posts_url(@post)
+    else
+     render :edit , status: 442
+    end
+    end
+   end
