c2897cdf7ec9575212f4cef92b6eebd4d877bfaa
shimaa <shimaafathi126@gmail.com>
2024-04-29 18:59:04 +0300

    update ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_030034) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_28_132342) do
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "content"
   end
 
 end
